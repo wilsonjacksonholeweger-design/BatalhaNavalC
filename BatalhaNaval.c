40aeb8b36da6402c5f08b04524e3fcb130e60c19
wilsonjacksonholeweger-design <wilsonjackson.holeweger@gmail.com>
2025-11-03 09:24:09 -0300

    Navios verticais

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,50 +1,75 @@
 #include <stdio.h>
 
+#define TAM 10
+#define NAVIO 3             // representa parte de um navio
+#define TAM_NAVIO 3         // cada navio ocupa 3 posições
+#define AGUA 0              // representa água
+
 int main(){
 
-    int tabuleiro[10][10];   // matriz do tabuleiro
-    int tamanhoNavio = 3;    // cada navio ocupa 3 posições
-    int agua = 0;            // representa água
-    int navio = 3;           // representa parte de um navio
+    int tabuleiro[TAM][TAM];   // matriz do tabuleiro
+               
 
     // Inicializa o tabuleiro com 0 (água)
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = agua;
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            tabuleiro[i][j] = AGUA;
         }
     }
 
     // Coordenadas iniciais dos navios
     // Navio 1: horizontal
-    int linhaH = 2;
+    int linhaH = 4;
     int colunaH = 4;
 
     // Navio 2: vertical
     int linhaV = 5;
     int colunaV = 1;
 
+    // Navios 3 e 4: diagonais
+    int linhaD1 = 0, colunaD1 = 0; // diagonal principal (↘)
+    int linhaD2 = 6, colunaD2 = 9; // diagonal secundária (↙)
+
 
     // Posiciona o navio horizontal
-    for (int i = 0; i < tamanhoNavio; i++) {
-        tabuleiro[linhaH][colunaH + i] = navio;
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linhaH][colunaH + i] = NAVIO;
     }
 
     // Posiciona o navio vertical
-    for (int i = 0; i < tamanhoNavio; i++) {
-        tabuleiro[linhaV + i][colunaV] = navio;
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linhaV + i][colunaV] = NAVIO;
+    }
+
+
+
+    // Navio 3: diagonal principal (↘)
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        if (tabuleiro[linhaD1 + i][colunaD1 + i] == NAVIO) {
+            
+        }
+        tabuleiro[linhaD1 + i][colunaD1 + i] = NAVIO;
+    }
+
+    // Navio 4: diagonal secundária (↙)
+    for (int i = 0; i < TAM_NAVIO; i++) {
+        if (tabuleiro[linhaD2 + i][colunaD2 - i] == NAVIO) {
+            
+        }
+        tabuleiro[linhaD2 + i][colunaD2 - i] = NAVIO;
     }
 
     // Exibe o tabuleiro na tela
     printf("\n=== TABULEIRO DE BATALHA NAVAL ===\n\n");
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
 
-    printf("\nLegenda: 0 = água | 3 = navio\n");
+    printf("\nLegenda: %d = água | %d = navio\n", AGUA, NAVIO);
 
     return 0;
 }
